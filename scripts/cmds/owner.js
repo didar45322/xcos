9575426e3ea781ea74e17355ef9a12abd665abe1
didar45322 <mdnoyon2010000@gmail.com>
2025-05-23 09:11:52 +0600

    Update owner.js

--- scripts/cmds/owner.js
+++ scripts/cmds/owner.js
@@ -16,14 +16,14 @@ config: {
   onStart: async function ({ api, event }) {
   try {
     const ownerInfo = {
-      name: '𝐇𝐔𝐒𝐒𝐀𝐈𝐍 💝👽',
+      name: 'Didar 🎀👽',
       choise: 'Only Russian 🥵🔥',
       habit: 'Playing Free Fire 🔥',
       gender: 'Male',
       age: '16+',
       height: '_hat 🐸🤠',
-      facebookLink: 'https://www.facebook.com/profile.php?id=100071009500533',
-      nick: '𝐇4𝐒𝐒𝐀1𝐍 💫🔥'
+      facebookLink: 'https://www.facebook.com/profile.php?id=100000934675181',
+      nick: '🕸️ mal 🕷️'
     };
 
     const bold = 'https://i.imgur.com/LbneO8C.mp4'; // Replace with your Google Drive videoid link https://drive.google.com/uc?export=download&id=here put your video id
